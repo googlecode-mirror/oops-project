64077cf5d48a50af5348c061bd64cefaf3af012b
rockmagic@yandex.ru <rockmagic@yandex.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-03-10 13:24:28 +0000

    Added demo and docs

---
+++ trunk/demos/tutorial/application/templates/_layout/header.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>The page title</title>
+<link rel="stylesheet" type="text/css" href="/css/s.css" />
+</head>
+
+<body>
+<div class="layout" id="header">
+	<h3>Layout header template</h3>
+	<div class="indexnumber">Template call index number: <b><?=$this->_num?></b></div>
+	<div class="tplname">called template name: <u><?=$this->_tplname?></u></i></div>
+	<div class="tplfile">found template file: <u><?=$this->_tplfile?></u></i></div>
+</div>

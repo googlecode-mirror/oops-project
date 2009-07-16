64ce32e071304eeea25e53b23c3581e2defb1495
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-07-16 16:47:56 +0000

    Process management architecture draft

---
+++ trunk/library/Oops/Process/Factory.php
@@ -0,0 +1,23 @@
+<?php
+
+class Oops_Process_Factory {
+	/**
+	 * Instantiate new process of a given class. Throws exception if class is not valid
+	 * 
+	 * @param string $processClass
+	 * @return Oops_Process
+	 * @throws Oops_Process_Exception
+	 */
+	static public function newProcess($processClass) {
+		
+	}
+	/**
+	 * 
+	 * @param string $pid
+	 * @return Oops_Process
+	 * @throws Oops_Process_Exception
+	 */
+	static public function getProcess($pid) {
+		
+	}
+}

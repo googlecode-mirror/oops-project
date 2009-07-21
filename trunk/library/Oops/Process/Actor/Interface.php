5f652376bd2dcc84c73bae605ae9dfcdea6cbb0e
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-07-21 09:58:13 +0000

    fixed controller params formatted some files

--- trunk/library/Oops/Process/Actor/Interface.php
+++ trunk/library/Oops/Process/Actor/Interface.php
@@ -1,3 +1,4 @@
 <?php 
 interface Oops_Process_Actor_Interface extends Oops_Process_Property_Interface {
+	public function __construct(Oops_Process_Abstract $process);
 }
--- trunk/library/Oops/Utils.php
+++ trunk/library/Oops/Utils.php
@@ -1,78 +1,81 @@
-<?
-/**
-* @package Oops
-*/
-
-if(!defined('OOPS_Loaded')) die("OOPS not found");
+<?php
 
 /**
-* Class for some special tasks
-*
-* @static
-*/
+ * @package Oops
+ * @license GPLv3
+ * @author Dmitry Ivanov rockmagic@yandex.ru
+ */
+
+/**
+ * Class for some special tasks
+ *
+ * @static
+ */
 class Oops_Utils {
+
 	/**
-	* Turms passed non-array variable to array containing the passed value
-	*
-	* @static
-	*/
+	 * Turms passed non-array variable to array containing the passed value
+	 *
+	 * @static
+	 */
 	public static function ToArray(&$a) {
 		if(is_array($a)) return;
-		if(!is_null($a) && $a!==false) {
-			$a=array($a);
+		if(!is_null($a) && $a !== false) {
+			$a = array ($a );
 			return;
 		}
-		$a=array();
+		$a = array ();
 	}
 
 	/**
-	* Turms passed non-array variable to array containing the passed value, and converts all values to integers
-	*
-	* @static
-	*/
-	public static function ToIntArray(&$a,$keepzero=false) {
+	 * Turms passed non-array variable to array containing the passed value, and converts all values to integers
+	 *
+	 * @static
+	 */
+	public static function ToIntArray(&$a, $keepzero = false) {
 		Oops_Utils::ToArray($a);
 		if(count($a)) {
-			foreach($a as $k=>$v) {
-				if(!(int)$v && (!$keepzero || $v!==(int)$v)) unset($a[$k]);
+			foreach($a as $k => $v) {
+				if(!(int) $v && (!$keepzero || $v !== (int) $v)) unset($a[$k]);
 			}
 		}
 	}
 
 	/**
-	* Turms passed non-array variable to array containing the passed value, and converts all values to integers
-	*
-	* @static
-	*/
+	 * Turms passed non-array variable to array containing the passed value, and converts all values to integers
+	 *
+	 * @static
+	 */
 	public static function ToNonEmptyArray(&$a) {
 		Oops_Utils::ToArray($a);
 		if(count($a)) {
-			foreach($a as $k=>$v) {
+			foreach($a as $k => $v) {
 				if(!$v) unset($a[$k]);
 			}
 		}
 	}
 
 	/**
-	* Same as ToIntArray but escapes all strings with mysql_real_escape_string
-	*
-	* @static
-	*/
-	public static function ToEscapedArray(&$a,$keepillegal=false) {
+	 * Same as ToIntArray but escapes all strings with mysql_real_escape_string
+	 *
+	 * @static
+	 */
+	public static function ToEscapedArray(&$a, $keepillegal = false) {
 		Oops_Utils::ToArray($a);
 		if(count($a)) {
-			foreach($a as $k=>$v) {
-				$v1=Oops_Sql::Escape($v);
-				if($v!=$v1) {
+			foreach($a as $k => $v) {
+				$v1 = Oops_Sql::Escape($v);
+				if($v != $v1) {
 					if(!$keepillegal) {
 						unset($a[$k]);
 					} else {
-						$a[$k]=$v1;
+						$a[$k] = $v1;
 					}
 				}
 			}
 		}
 	}
+
 	/**
 	 * Compile a given Tree to one-dimensional List having a depth level set for each element
 	 * 
@@ -86,12 +89,12 @@ class Oops_Utils {
 	 * Используется:
 	 * comments_assembler::ToLine();
 	 */
-	public static function Tree2Line(&$Tree, $childrenId="Children", $levelId='Level'){
-		$ret=array();
+	public static function Tree2Line(&$Tree, $childrenId = "Children", $levelId = 'Level') {
+		$ret = array ();
 		Oops_Utils::_Tree2Line($ret, $Tree, $childrenId, $levelId, 0);
 		return $ret;
 	}
-	
+
 	/**
 	 * @ignore
 	 *
@@ -103,58 +106,59 @@ class Oops_Utils {
 	 * @author cloud
 	 * @access public
 	 */
-	protected static function _Tree2Line(&$ret, &$Tree, $childrenId, $levelId, $Level=0){
-		if ($count=count($Tree)){
-			for ($i=0; $i<$count; $i++){					
-				$ret[]=$Tree[$i];
-				unset($ret[count($ret)-1][$childrenId]);
-				$ret[count($ret)-1][$childrenId][$levelId]=$Level;
-				if (isset($Tree[$i][$childrenId])){					
-					Utils::_Tree2Line($ret, $Tree[$i][$childrenId], $childrenId, $levelId, $Level+1);
+	protected static function _Tree2Line(&$ret, &$Tree, $childrenId, $levelId, $Level = 0) {
+		if($count = count($Tree)) {
+			for($i = 0; $i < $count; $i++) {
+				$ret[] = $Tree[$i];
+				unset($ret[count($ret) - 1][$childrenId]);
+				$ret[count($ret) - 1][$childrenId][$levelId] = $Level;
+				if(isset($Tree[$i][$childrenId])) {
+					Utils::_Tree2Line($ret, $Tree[$i][$childrenId], $childrenId, $levelId, $Level + 1);
 				}
 			}
 		}
 	}
 
-
 	/**
-	* @param array Line array
-	* @param string parentid key name
-	* @param string childrenid key name
-	* @return array
-	*/
-	public static function Line2Tree($Line,$ParentID='parent',$childrenId='children',$skipIfNoKey=false) {
-		$ret = array();
-		foreach($Line as $k=>$v) {
+	 * @param array Line array
+	 * @param string parentid key name
+	 * @param string childrenid key name
+	 * @return array
+	 */
+	public static function Line2Tree($Line, $ParentID = 'parent', $childrenId = 'children', $skipIfNoKey = false) {
+		$ret = array ();
+		foreach($Line as $k => $v) {
 			if(!isset($v[$ParentID])) { //NULL is like !isset - No Parent
-				$Line[$k][$ParentID]=false;
+				$Line[$k][$ParentID] = false;
 			}
-			$Parent =& $Line[$k][$ParentID];
-			if($Parent!==false && !isset($Line[$Parent])) {
-				$Line[$k][$ParentID]=false;
+			$Parent = & $Line[$k][$ParentID];
+			if($Parent !== false && !isset($Line[$Parent])) {
+				$Line[$k][$ParentID] = false;
 			}
 			if(!$Parent) {
-				$ret[$k] =& $Line[$k];
+				$ret[$k] = & $Line[$k];
 				continue;
 			}
-			if(!isset($Line[$Parent][$childrenId]) || !is_array($Line[$Parent][$childrenId])) $Line[$Parent][$childrenId]=array();
-			$Line[$Parent][$childrenId][$k]=&$Line[$k];
+			if(!isset($Line[$Parent][$childrenId]) || !is_array($Line[$Parent][$childrenId])) $Line[$Parent][$childrenId] = array ();
+			$Line[$Parent][$childrenId][$k] = &$Line[$k];
 		}
-		if(true && $skipIfNoKey!==false) {
-			$dontskip=array();
-			foreach($Line as $k=>$v) {
-				if(array_key_exists($skipIfNoKey,$v)) {
+		if(true && $skipIfNoKey !== false) {
+			$dontskip = array ();
+			foreach($Line as $k => $v) {
+				if(array_key_exists($skipIfNoKey, $v)) {
 					while($k) {
-						$dontskip[]=$k;
+						$dontskip[] = $k;
 						$k = $Line[$k][$ParentID];
 					}
 				}
 			}
-			foreach($Line as $k=>$v) {
-				if(!in_array($k,$dontskip)) {
+			foreach($Line as $k => $v) {
+				if(!in_array($k, $dontskip)) {
 					unset($Line[$k]);
-					if($v[$ParentID]) unset($Line[$v[$ParentID]][$childrenId][$k]);
-					else unset($ret[$k]);
+					if($v[$ParentID])
+						unset($Line[$v[$ParentID]][$childrenId][$k]);
+					else
+						unset($ret[$k]);
 				}
 			}
 		}

d83b72d3de81867cf6065b7c8eed0cad99f4e3b1
di <di@mail.nnov.ru>
2009-08-01 16:05:29 +0000

    Abstract process class

---
+++ library/Oops/Process/Storage.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 
+ * @author Dmitry Ivanov
+ * Queued process data retrieval and storing
+ *
+ */
+class Oops_Process_Storage {
+
+	/**
+	 * 
+	 * @param string $pid Process ID
+	 * @return array Stored process data array
+	 */
+	public function get($pid) {
+	}
+
+	public function set($pid, $data) {
+	}
+
+	public function insert($pid, $data) {
+	}
+
+	public function prepare(array $pids) {
+	
+	}
+}

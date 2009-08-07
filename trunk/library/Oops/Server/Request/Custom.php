eeca31bfc42f56089016a891424d5c8b0dc4a6e5
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-08-07 12:07:24 +0000

    Fixed bug in Oops/Server/Request/Custom.php with undefined property _get

--- trunk/library/Oops/Server/Request/Custom.php
+++ trunk/library/Oops/Server/Request/Custom.php
@@ -14,12 +14,13 @@ require_once("Oops/Server/Request.php");
 * Internal (sub)request representation object (oops://local/path/to/resource/action.view)
 */
 class Oops_Server_Request_Custom extends Oops_Server_Request {
+	protected $_get = array();
 
 	function __construct($url) {
 		$parsed = parse_url($url);
 		foreach($parsed as $name=>$value) $this->$name = $value;
 
-		parse_str($this->query,$this->_get);
+		parse_str($this->query, $this->_get);
 		$this->_params = $this->_get;
 	}
 }

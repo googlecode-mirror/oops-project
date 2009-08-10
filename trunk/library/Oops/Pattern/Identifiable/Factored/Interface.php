07756d75bcd8940d9821b57d1edc131d84b2e0e2
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-08-10 07:25:26 +0000

    getId method for identifiable interface Some comments and require_once in patterns

--- trunk/library/Oops/Pattern/Identifiable/Factored/Interface.php
+++ trunk/library/Oops/Pattern/Identifiable/Factored/Interface.php
@@ -1,4 +1,5 @@
 <?php
+require_once("Oops/Pattern/Identifiable/Interface.php");
 
 interface Oops_Pattern_Identifiable_Factored_Interface extends Oops_Pattern_Identifiable_Interface {
 	public static function getFactoryCallback();

ec9381baf676e0916c8fa539e5f6403e4327f7a3
di <di@mail.nnov.ru>
2009-08-12 15:37:03 +0000

    Some changes in Form attached data logic

--- library/Oops/Form/Dispatcher.php
+++ library/Oops/Form/Dispatcher.php
@@ -7,6 +7,12 @@ class Oops_Form_Dispatcher extends Oops_Event_Dispatcher {
 		parent::__construct($name);
 	}
 
+	/**
+	 * (non-PHPdoc)
+	 * @see Oops/Event/Oops_Event_Dispatcher#post($event, $info)
+	 * 
+	 * @return Oops_Form_Notification
+	 */
 	function &post($event, $info = array()) {
 		$notification = new Oops_Form_Notification($event, $info);
 		return $this->postNotification($notification);

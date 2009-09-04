6c851999a74eef8ff7ed9dbce99d19c950b3bbd7
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-09-04 11:08:47 +0000

    Starting unit tests skeleton

---
+++ trunk/tests/AllTests.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'tests_Oops_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'UtilsTests.php';
+
+class tests_Oops_AllTests {
+    // {{{ main()
+
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    // }}}
+    // {{{ suite()
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Oops tests');
+        $suite->addTestSuite('tests_Utils');
+
+        return $suite;
+    }
+
+    // }}}
+}
+
+if (PHPUnit_MAIN_METHOD == 'tests_Oops_AllTests::main') {
+    tests_Oops_AllTests::main();
+}

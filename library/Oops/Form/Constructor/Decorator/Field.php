b64eb25b3136edf6add4e066a7ce9ce1c3b6c547
kirill.a.egorov <kirill.a.egorov@gmail.com>
2009-09-09 13:07:32 +0000

    Erorr Decorator

--- library/Oops/Form/Constructor/Decorator/Field.php
+++ library/Oops/Form/Constructor/Decorator/Field.php
@@ -1,7 +1,7 @@
 <?php
 class Oops_Form_Constructor_Decorator_Field extends Oops_Form_Constructor_Decorator
 {
-    public function createElement($name, $text, $html)
+    public function createElement($name, $text, $html, $errors = false)
     {
         return '<label>' . $text . '</label>' . $html .'<br>'."\n";
     }
--- library/Oops/Form/Constructor/Decorator/Group.php
+++ library/Oops/Form/Constructor/Decorator/Group.php
@@ -1,7 +1,7 @@
 <?php
 class Oops_Form_Constructor_Decorator_Group extends Oops_Form_Constructor_Decorator
 {
-   public function createElement($name, $text, $html)
+   public function createElement($name, $text, $html, $errors = false)
     {
         return '<fieldset><legend>' . $text . '</legend>' . $html .'</fieldset>' . "\n";
     }

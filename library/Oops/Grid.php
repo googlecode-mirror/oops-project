75df2a95d49221c63b739ff946f4178fa5c27689
kirill.a.egorov <kirill.a.egorov@gmail.com>
2009-09-14 09:18:13 +0000

    Code comments

--- library/Oops/Grid.php
+++ library/Oops/Grid.php
@@ -1,17 +1,54 @@
 <?php
 class Oops_Grid
 {
+    /*
+     * th decorator const name
+     */
     const decoratorHeadItem = 1;
+    
+    /*
+     * tr decorator const name
+     */
     const decoratorRow = 2;
+    
+    /*
+     * td decorator const name
+     */
     const decoratorItem = 3;
      
+    /*
+     * Grid data array
+     */
     protected $_data;
+    
+    /*
+     * Grid titles array
+     */
     protected $_titles;
+    
+    /*
+     * Grid html result
+     */
     protected $_result;
+    
+    /*
+     * Table th decorator object
+     */
     protected $_headItemDecorator;
+    
+    /*
+     * Table tr decorator object
+     */
     protected $_rowDecorator;
+    
+    /*
+     * Table td decorator object
+     */
     protected $_itemDecorator;
     
+    /*
+     * Table css class
+     */
     public $class = 'table';
     
     
@@ -20,6 +57,10 @@ class Oops_Grid
        $this->clear();
     }
     
+    /**
+     * Clear grid
+     * @return void
+     */
     public function clear()
     {
         $this->_data = array();
@@ -27,19 +68,30 @@ class Oops_Grid
         $this->_result = '';
     }
     
+    /**
+     * @param array $data
+     * @return void
+     */
     public function setData(array $data)
     {
         $this->_data = $data;
     }
+    
+    /**
+     * @param array $titles
+     * @return void
+     */
     public function setTitles(array $titles)
     {
         $this->_titles = $titles;
     }
+    
     public function __tostring()
     {
          $this->_make();
          return $this->_result;   
     }
+    
     protected function _checkDecorators()
     {
         if(!is_object($this->_headItemDecorator))
@@ -51,6 +103,7 @@ class Oops_Grid
         if(!is_object($this->_itemDecorator))
             $this->_itemDecorator = new Oops_Grid_Decorator_Item();
     }
+    
     protected function _make()
     {
         $this->_checkDecorators();
@@ -107,6 +160,11 @@ class Oops_Grid
         $this->_result.='</table>';       
     }
     
+    /**
+     * @param const Oops_Grid_Decorator
+     * @param Oops_Grid_Decorator obj
+     * @return void
+     */
     public function setDecorator($decorator ,Oops_Grid_Decorator $obj )
     {
         switch($decorator)

f42bed9ecd8d6ea616b25145f3c415d4af9c4ebd
kirill.a.egorov <kirill.a.egorov@gmail.com>
2009-09-17 04:49:30 +0000

    titles check

--- library/Oops/Grid.php
+++ library/Oops/Grid.php
@@ -51,6 +51,27 @@ class Oops_Grid
      */
     public $class = 'table';
     
+    /*
+     * Use ExtJs Grid component
+     */
+    public $isExt = false;
+    
+    /*
+     *  Ext Js Grid params
+     *  array(	
+     *  		'id'=>'',
+     *  		'dataUrl'=>'',
+     *  		'idField'=>'',
+     *  		'data'=>''
+     *  )
+     */
+    public $extParams;
+    
+    /*
+     * Ext Js grid config array
+     */
+    protected $_extData;
+    
     
     public function __construct()
     {
@@ -88,10 +109,102 @@ class Oops_Grid
     
     public function __tostring()
     {
-         $this->_make();
+        if($this->isExt)
+             $this->_makeExtGrid();
+        else 
+             $this->_make();
+             
          return $this->_result;   
     }
-    
+    protected function _makeExtGrid()
+    {
+        if(empty($this->extParams))
+            throw new Exception('Need ext js params');
+            
+        if(empty($this->_data)) 
+            return;  
+
+        $this->_checkDecorators();
+        $this->_checkTitles();
+        
+        if(!isset($this->extParams['columns']))
+            $this->extParams['columns'] = array();
+        
+        $this->_extData['columns'] = '';
+        $this->_extData['readerFields']='';    
+         foreach($this->_titles as $k=>$v)
+         {
+                 $width = 160;
+                 $sortable = 'true';
+                 $dataIndex = $k;
+                 
+             if(isset($this->extParams['columns'][$k]))
+             {
+                 $colConfig = $this->extParams['columns'][$k];
+                 if(isset($colConfig['width']))
+                     $width = $colConfig['width'];
+                 if(isset($colConfig['sortable']))
+                     $width = $colConfig['sortable'];
+                 if(isset($colConfig['dataIndex']))
+                     $width = $colConfig['dataIndex'];         
+             }
+
+             $this->_extData['columns'].= '{id:\''.$k.'\',header: \''.$v.'\', width: '.$width.', sortable: '.$sortable.', dataIndex: \''.$dataIndex.'\'},
+             ';
+             
+             if($k===$this->extParams['idField'])
+                 $this->_extData['readerFields'].='{name: \''.$k.'\'},
+                 ';
+             else
+                 $this->_extData['readerFields'].='{name: \''.$k.'\', allowBlank: false},
+                 ';    
+         }
+            
+
+         $this->_extData['store'] = '
+         var reader = new Ext.data.JsonReader(
+         						{
+                                    totalProperty: \''.$this->extParams['totalProperty'].'\',
+                                    successProperty: \''.$this->extParams['successProperty'].'\',
+                                    idProperty: \''.$this->extParams['idField'].'\',
+                                    root: \''.$this->extParams['data'].'\',
+                            	}, 
+                            	[
+                            	'.$this->_extData['readerFields'].']
+                          );
+               
+         var store = new Ext.data.Store({
+                                    url: \''.$this->extParams['dataUrl'].'\',
+                                    reader: reader,
+        				});
+        ';
+         
+         $this->_result = '
+         <script language="javascript">
+         	Ext.onReady(function(){
+         	
+         		'.$this->_extData['store'].'
+         		
+         		store.loadData();
+         		
+         	    var grid = new Ext.grid.GridPanel({
+                                    store: store,
+                                    columns: [
+                                    '.$this->_extData['columns'].'
+                                    ],
+                                    stripeRows: true,
+                                   // autoExpandColumn: \'company\',
+                                    height: 350,
+                                    width: 600,
+                                    title: \'Grid\',                             
+                                    stateful: true,
+                                    stateId: \'grid\',        
+                            	});
+                            
+               grid.render(\''.$this->extParams['id'].'\');       	
+         	}
+         </script>';
+    }
     protected function _checkDecorators()
     {
         if(!is_object($this->_headItemDecorator))
@@ -103,23 +216,19 @@ class Oops_Grid
         if(!is_object($this->_itemDecorator))
             $this->_itemDecorator = new Oops_Grid_Decorator_Item();
     }
-    
+    protected function _checkTitles()
+    {
+        if(empty($this->_titles)) 
+            throw new Exception('No titles');
+    }
     protected function _make()
     {
-        $this->_checkDecorators();
-            
         if(empty($this->_data)) 
-            return;   
-            
-        if(empty($this->_titles)) 
-        {
-            $key = key($this->data);
-            foreach($this->_data[$key] as $v)
-                $this->_titles[] = $v;
-            
-             reset($this->_data);   
-        }   
-        
+            return;  
+
+        $this->_checkDecorators();
+        $this->_checkTitles();
+              
         $this->_result = '<table class="'.$this->class.'">';
         
         
@@ -143,9 +252,12 @@ class Oops_Grid
         $k=0;
         foreach ($this->_data as $rkey=>$value)   
         {
+            
+                
             $row = '';
             foreach ($value as $key=>$item)
-                $row.=$this->_itemDecorator->createElement($key,$item,false);
+                if(isset($this->_titles[$key]))
+                    $row.=$this->_itemDecorator->createElement($key,$item,false);
                                
             if($k==1){
                 $key = true;

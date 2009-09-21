c9c9e799d2d8e2ad2655c69289be3ebe3ef2c058
kirill.a.egorov@gmail.com <kirill.a.egorov@gmail.com@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-09-21 11:48:21 +0000

    Object field fix

--- trunk/library/Oops/Form/Field/Object.php
+++ trunk/library/Oops/Form/Field/Object.php
@@ -16,6 +16,10 @@ class Oops_Form_Field_Object extends Oops_Form_Field
     }
     protected function _make()
     {        
-        $this->html = '<input type="hidden" ' . join(' ', $this->_params) . ' ><input type="text" id="' . $this->_extra['id'] . '_title" value="'.$this->_title.'" /><input type="button" value="select" onClick="registry_showSelectObject(\'' . $this->_refClass . '\',\'' . $this->_extra['id'] . '\')" >';
+       // $this->html = '<input type="hidden" ' . join(' ', $this->_params) . ' ><input type="text" id="' . $this->_extra['id'] . '_title" value="'.$this->_title.'" /><input type="button" value="select" onClick="registry_showSelectObject(\'' . $this->_refClass . '\',\'' . $this->_extra['id'] . '\')" >';
+        $this->html = '<input type="hidden" ' . join(' ', $this->_params)
+				. ' ><span class="linkvalue exec registry_showselectobject ' . $this->_refClass . ' '
+				. $this->_extra['id'] . '" id="' . $this->_extra['id'] . '_title">'
+				. ((strlen($this->title)) ? $this->title : '-') . '</span>';	
     }
 }

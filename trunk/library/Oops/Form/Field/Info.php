77114ba3e9c97728a566312a03c35b61b7a2cbe9
kirill.a.egorov@gmail.com <kirill.a.egorov@gmail.com@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-09-21 09:06:46 +0000

    Info field Fix

--- trunk/library/Oops/Form/Field/Info.php
+++ trunk/library/Oops/Form/Field/Info.php
@@ -3,6 +3,6 @@ class Oops_Form_Field_Info extends Oops_Form_Field
 {
     protected function _make()
     {
-        $this->html='<label ' . join(' ', $this->_params) . ' > ' . $this->_value . '</label>';
+        $this->html = $this->_value;
     }
 }

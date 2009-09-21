88eaf734519277cf4cb79ed586853e6cdc084ff5
kirill.a.egorov <kirill.a.egorov@gmail.com>
2009-09-21 11:46:47 +0000

    Object field fix

--- library/Oops/Form/Field/Object.php
+++ library/Oops/Form/Field/Object.php
@@ -9,13 +9,13 @@ class Oops_Form_Field_Object extends Oops_Form_Field
         if(!isset($extra['id']))
 	        $extra['id'] = 'id_'.$name;
 	        
-        parent::__construct($name,$class,$value,$extra);   
+        parent::__construct($name,$value,$class,$extra);   
         
         $this->_title = $title;
         $this->_refClass = $refClass;   
     }
     protected function _make()
-    {
-        $html = '<input type="hidden" ' . join(' ', $this->_params) . ' ><input type="text" id="' . $this->_extra['id'] . '_title" value="'.$this->_title.'" /><input type="button" value="select" onClick="registry_showSelectObject(\'' . $this->_refClass . '\',\'' . $this->_extra['id'] . '\')" >';
+    {        
+        $this->html = '<input type="hidden" ' . join(' ', $this->_params) . ' ><input type="text" id="' . $this->_extra['id'] . '_title" value="'.$this->_title.'" /><input type="button" value="select" onClick="registry_showSelectObject(\'' . $this->_refClass . '\',\'' . $this->_extra['id'] . '\')" >';
     }
 }

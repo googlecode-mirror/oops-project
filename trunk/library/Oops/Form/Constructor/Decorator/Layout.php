f17ed964e8067a2521d2b763e1b62b7912e33ce0
kirill.a.egorov@gmail.com <kirill.a.egorov@gmail.com@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-09-23 06:59:30 +0000

    Forms Layout

--- trunk/library/Oops/Form/Constructor/Decorator/Layout.php
+++ trunk/library/Oops/Form/Constructor/Decorator/Layout.php
@@ -1,7 +1,7 @@
 <?php
 class Oops_Form_Constructor_Decorator_layout extends Oops_Form_Constructor_Decorator
 {
-   public function createElement($name, $text, $html, $errors = false)
+    public function createElement($name, $text, $html, $errors = false)
     {
         return $html;
     }

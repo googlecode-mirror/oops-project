f506d94902a64be66728d677f2e24c6eb671ff4b
rockmagic.net <rockmagic.net@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-09-27 12:35:09 +0000

    Added Oops_Server_Request_Clone for request management

---
+++ library/Oops/Server/Request/Clone.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Class for custom request configuration
+ * 
+ * @author Dmitry Ivanov
+ *
+ */
+class Oops_Server_Request_Clone extends Oops_Server_Request {
+	public function __construct(Oops_Server_Request $request) {
+		foreach(get_object_vars($request) as $k=>$v) {
+			$this->$k = $request->$k;
+		}
+	}
+	
+	public function __set($name, $value) {
+		$this->_params[$name] = $value;
+	}
+	
+	public function offsetSet($offset, $value) {
+		debugPrint($value, $offset);
+		$this->_params[$offset] = $value;
+	}
+}

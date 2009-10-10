af2d9f56e7a02ed0b924dc9fca979bad1ddfa0df
rockmagic.net <rockmagic.net@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-10-10 13:23:17 +0000

    Added test for Oops_Config_Writer_Ini

--- tests/ConfigTests.php
+++ tests/ConfigTests.php
@@ -29,6 +29,37 @@ class tests_Config extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals('another Section Value', $config->another_section->sectionKey);
 	}
+
+	function test_ConfigWriterIni() {
+		require_once 'Oops/Config/Ini.php';
+		require_once 'Oops/Config/Writer/Ini.php';
+		$config = new Oops_Config_Ini('./resources/sampleConfig.ini', '.', true);
+		$config->newSection = array('newKey1' => 'newValue1', 'newKey2' => 'newValue2');
+		$config->newRootKey = 'New root value';
+		
+		$writer = new Oops_Config_Writer_Ini();
+		$writer->setFilename('./resources/sampleConfigWritten.ini');
+		$writer->setConfig($config);
+
+		$this->assertTrue($writer->write());
+
+		$written = new Oops_Config_Ini('./resources/sampleConfigWritten.ini');
+		
+		//Check new values
+		$this->assertEquals($written->newSection->newKey1, 'newValue1');
+		$this->assertEquals($written->newSection->newKey2, 'newValue2');
+		$this->assertEquals($written->newRootKey, 'New root value');
+		
+		$this->assertEquals('value', $written->key);
+		$this->assertEquals(true, (bool) $written->booleanTrue);
+		$this->assertEquals(false, (bool) $written->booleanFalse);
+		
+		$this->assertEquals('sectionValue', $written->section->sectionKey);
+		$this->assertEquals('delimitedValue1', $written->section->delimited->key1);
+		$this->assertEquals('delimitedValue2', $written->section->delimited->key2);
+		
+		unlink('./resources/sampleConfigWritten.ini');
+	}
 	// }}}
 }
 

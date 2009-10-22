1ed799b15c9c9c00280049f7d1bd94531946000f
rockmagic.net <rockmagic.net@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-10-22 09:43:42 +0000

    Added isFile property to Form_Field

--- trunk/library/Oops/Form/Field/File.php
+++ trunk/library/Oops/Form/Field/File.php
@@ -1,6 +1,8 @@
 <?php
 
 class Oops_Form_Field_File extends Oops_Form_Field {
+	
+	protected $_isFile = true;
 
 	public function __construct($name, $value, $class, $extra) {
 		parent::__construct($name, $value, $class, $extra);

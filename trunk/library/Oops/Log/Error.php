f866b5afaa76857292142402f7a72fca48d6cfe5
di@mail.nnov.ru <di@mail.nnov.ru@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2009-11-10 18:25:25 +0000

    Added server errors logging to file

---
+++ trunk/library/Oops/Log/Error.php
@@ -0,0 +1,30 @@
+<?php
+
+class Oops_Log_Error {
+	public static function report(Oops_Error_Handler $errorHandler, $path) {
+		$errors = '';
+		foreach($errorHandler->getErrors() as $err)
+			$errors .= "Oops-Error: $err\n";
+		foreach($errorHandler->getWarnings() as $err)
+			$errors .= "Oops-Warning: $err\n";
+		foreach($errorHandler->getNotices() as $err)
+			$errors .= "Oops-Notice: $err\n";
+		foreach($errorHandler->getPhps() as $err)
+			$errors .= "Php-errors: $err\n";
+
+		if(!strlen($errors)) return;
+
+		$logFile = new Oops_File($path . "/error.log");
+		if($logFile->size > 50*1024) {
+			$logFile->rename($logFile->filename . ".1");
+			$logFile = new Oops_File($path . "/error.log");
+		}
+		
+		$logFile->makeWriteable();
+		$ft = fopen($logFile->filename, "a");
+
+		$request = Oops_Server::getRequest();
+		fputs($ft, "\n" . date("r") . "\n " . $request->getUrl() ."\n" . $errors);
+		fclose($ft);
+	}
+}

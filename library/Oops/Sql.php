fc7a4ec5fb33d89b5c873508a3afe1dbebef625a
di <di@mail.nnov.ru>
2010-02-08 12:07:10 +0000

    Added Oops_Sql::setLink to use Oops_Sql tools from other frameworks managing it's connections itself

--- library/Oops/Sql.php
+++ library/Oops/Sql.php
@@ -11,20 +11,20 @@ define("OOPS_SQL_EXCEPTION", 1);
  * MySQL connection and query functionality
  */
 class Oops_Sql {
-
+	
 	/**
 	 * MySQL connection resource
 	 * @var resource
 	 */
 	protected static $_link;
-
+	
 	/**
 	 * MySQL configuration
 	 *
 	 * @var Oops_Config
 	 */
 	protected static $_config;
-
+	
 	/**
 	 * True if configuration initialization complete
 	 *
@@ -41,6 +41,16 @@ class Oops_Sql {
 		return self::$_link;
 	}
 
+	/**
+	 * Sets MySQL connection for Oops. This method should be used before any query or Oops_Sql_Utils call if you handle connection by your own module  
+	 */
+	public static function setLink($link) {
+		if(is_resource($link) && get_resource_type($link) == 'mysql link') self::$_link = $link;
+	}
+
+	/**
+	 * Init Oops_Sql configuration using Oops_Server environment
+	 */
 	protected static function _Init() {
 		if(self::$_initComplete) return;
 		self::$_initComplete = true;
@@ -71,19 +81,19 @@ class Oops_Sql {
 	 * @return resource MySQL link
 	 */
 	public static function Connect() {
-		self::_Init();
-
 		if(!is_resource(self::$_link)) {
+			self::_Init();
+			
 			self::$_link = mysql_connect(self::$_config->host, self::$_config->user, self::$_config->password);
-				
+			
 			if(!is_resource(self::$_link)) Oops_Sql::_initError("mysql_connect");
-				
+			
 			$database = @self::$_config->database;
 			if(strlen($database)) {
 				$result = mysql_select_db($database, self::$_link);
 				if(!$result) Oops_Sql::_initError("mysql_select_db/" . $database);
 			}
-				
+			
 			$names = @self::$_config->names;
 			if(strlen($names)) mysql_query("SET NAMES " . $names, self::$_link);
 		}
@@ -96,29 +106,29 @@ class Oops_Sql {
 	public static function Query($query, $dieOnError = false) {
 		// @todo Consider using event dispatcher to run logger, adding listener for onBeforeSqlQuery inside the _init function
 		Oops_Sql::Connect();
-
+		
 		static $loggerEnabled = null;
 		if(!isset($loggerEnabled)) {
 			if(is_object(self::$_config->logger))
-			$loggerEnabled = self::$_config->logger->enabled;
+				$loggerEnabled = self::$_config->logger->enabled;
 			else
-			$loggerEnabled = false;
+				$loggerEnabled = false;
 		}
-
+		
 		if($loggerEnabled) {
 			require_once ('Oops/Sql/Logger.php');
 			static $l = null;
 			if(!is_object($l)) $l = Oops_Sql_Logger::getInstance(self::$_config->logger->table);
-				
+			
 			if(self::$_config->logger->probability > mt_rand(0, 1)) {
 				return $l->Analyze($query);
 			}
 		}
-
+		
 		$result = mysql_query($query, self::$_link);
-
+		
 		if(mysql_errno(self::$_link)) return self::Error($dieOnError, $query);
-
+		
 		return $result;
 	}
 

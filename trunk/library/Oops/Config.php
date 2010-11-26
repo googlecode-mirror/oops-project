9807209350c04b46f149d56d89e5180a067eabc6
rockmagic.net@gmail.com <rockmagic.net@gmail.com@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2010-11-26 14:27:37 +0000

    Lazy initialization and initial data definition for Oops_Config

--- trunk/library/Oops/Config.php
+++ trunk/library/Oops/Config.php
@@ -13,10 +13,18 @@
  */
 class Oops_Config implements Countable, Iterator {
 	protected $_data = array();
+	protected $_initialData = array();
 	public $used = false;
 	protected $_cache = array();
 	protected $_arrayPosition = 0;
 	protected $_keyDelimiter;
+	
+	/**
+	 * Config init state (late init used)
+	 * 
+	 * @var bool
+	 */
+	protected $_initComplete = false;
 	
 	/**
 	 * Whether in-memory modifications to configuration data are allowed
@@ -35,11 +43,22 @@ class Oops_Config implements Countable, Iterator {
 		
 		$this->_keyDelimiter = $keyDelimiter;
 		$this->_allowModifications = $allowModifications;
+		if(count($data)) {
+			if(count($this->_initialData))
+				$this->_initialData = array_merge($this->_initialData, $data);
+			else
+				$this->_initialData = $data;
+		}
+	}
+
+	protected function _init() {
+		if($this->_initComplete) return;
+		$this->_initComplete = true;
 		
-		foreach($data as $key => $value) {
+		foreach($this->_initialData as $key => $value) {
 			$key = strtolower($key);
-			if(strpos($key, $keyDelimiter) !== false) {
-				list($key, $subKey) = explode($keyDelimiter, $key, 2);
+			if(strpos($key, $this->_keyDelimiter) !== false) {
+				list($key, $subKey) = explode($this->_keyDelimiter, $key, 2);
 				$value = array($subKey => $value);
 			}
 			if(is_array($value)) {
@@ -57,17 +76,33 @@ class Oops_Config implements Countable, Iterator {
 			else
 				trigger_error("Config/ConflictingInitValues/$key", E_USER_WARNING);
 		}
+		
+		// cleaning up memory
+		unset($this->_initialData);
 	}
 
 	function getKeys() {
+		$this->_init();
 		return array_keys($this->_data);
 	}
 
 	function get($key) {
+		return $this->__get($key);
+	}
+
+	/**
+	 * Getter
+	 * @ignore
+	 */
+	function __get($key) {
 		if(!strlen((string) $key)) {
 			trigger_error("Config/InvalidConfigKey/$key", E_USER_WARNING);
 			return null;
 		}
+		
+		// Check if init complete here to avoid numerous method calls
+		if(!$this->_initComplete) $this->_init();
+		
 		$key = strtolower($key);
 		if(!isset($this->_data[$key])) {
 			require_once 'Oops/Config/Fake.php';
@@ -77,29 +112,25 @@ class Oops_Config implements Countable, Iterator {
 	}
 
 	/**
-	 * Getter
-	 * @ignore
-	 */
-	function __get($var) {
-		return $this->get($var);
-	}
-
-	/**
 	 * Setter
 	 * @ignore
 	 */
 	function __set($var, $value) {
 		if(!$this->_allowModifications) return false;
-
+		
 		if(!strlen((string) $var)) {
 			trigger_error("Config/InvalidConfigKey/$var", E_USER_WARNING);
 			return null;
 		}
+		
 		$var = strtolower($var);
 		
 		if(is_array($value)) {
 			$value = new Oops_Config($value, $this->_keyDelimiter, $this->_allowModifications);
 		}
+		
+		// Check if init complete here to avoid numerous method calls
+		if(!$this->_initComplete) $this->_init();
 		
 		if(isset($this->_data[$var]) && $this->_data[$var] instanceof Oops_Config_Fake) {
 			$fakeData = $this->_data[$var]->getData();
@@ -125,6 +156,10 @@ class Oops_Config implements Countable, Iterator {
 	 */
 	public function mergeConfig($config) {
 		if(!is_object($config)) return;
+		
+		$this->_init();
+		$config->_init();
+		
 		foreach($config->_data as $key => $value) {
 			if(!isset($this->_data[$key])) {
 				$this->_data[$key] = $config->_data[$key];
@@ -141,6 +176,7 @@ class Oops_Config implements Countable, Iterator {
 	}
 
 	public function count() {
+		$this->_init();
 		return count($this->_data);
 	}
 
@@ -148,29 +184,35 @@ class Oops_Config implements Countable, Iterator {
 	 * Iterator interface method
 	 */
 	public function current() {
+		$this->_init();
 		return current($this->_data);
 	}
 
 	public function key() {
+		$this->_init();
 		return key($this->_data);
 	}
 
 	public function next() {
+		$this->_init();
 		++$this->_arrayPosition;
 		next($this->_data);
 	}
 
 	public function rewind() {
+		$this->_init();
 		$this->_arrayPosition = 0;
 		reset($this->_data);
 	}
 
 	public function valid() {
+		$this->_init();
 		if($this->_arrayPosition < count($this->_data) && $this->_arrayPosition >= 0) return true;
 		return false;
 	}
 
 	public function __toArray() {
+		$this->_init();
 		$array = $this->_data;
 		foreach($array as $k => $v) {
 			if($v instanceof Oops_Config) $array[$k] = $v->__toArray();

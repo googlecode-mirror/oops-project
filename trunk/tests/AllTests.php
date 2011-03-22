4a6a033a4c5dab0bbcd7680fb024608db8b15aab
rockmagic.net@gmail.com <rockmagic.net@gmail.com@d6b5fa1a-05a6-11de-b321-f573fdaff03e>
2011-03-22 00:33:28 +0000

    added some tests

--- trunk/tests/AllTests.php
+++ trunk/tests/AllTests.php
@@ -3,8 +3,8 @@ if(!defined('PHPUnit_MAIN_METHOD')) {
 	define('PHPUnit_MAIN_METHOD', 'tests_Oops_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once 'PHPUnit/Autoload.php';
+//require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once 'UtilsTests.php';
 require_once 'ConfigTests.php';
 require_once 'ServerTests.php';
@@ -12,6 +12,8 @@ require_once 'RequestTests.php';
 require_once 'ResponseTests.php';
 require_once 'FileUtilsTests.php';
 require_once 'ValidateTests.php';
+require_once 'Oops_Sql_SelectorTest.php';
+require_once 'Oops_Uri_ParserTest.php';
 
 class tests_Oops_AllTests {
 
@@ -33,6 +35,8 @@ class tests_Oops_AllTests {
 		$suite->addTestSuite('tests_Response');
 		$suite->addTestSuite('tests_FileUtils');
 		$suite->addTestSuite('tests_Validate');
+		$suite->addTestSuite('Oops_Sql_SelectorTest');
+		$suite->addTestSuite('Oops_Uri_ParserTest');
 		
 		return $suite;
 	}

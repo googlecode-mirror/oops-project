9bfce73d5969a6991c5a10c4fb1c10710839f11a
rockmagic.net <rockmagic.net@gmail.com>
2012-07-18 11:16:55 +0000

    New feature Oops_Sql_Common::insertUpdate - INSERT INTO ... ON DUPLICATE KEY UPDATE ...

--- library/Oops/Session/Database.php
+++ library/Oops/Session/Database.php
@@ -1,57 +1,59 @@
 <?php
 /**
-* @package Oops
-*/
+ * @package Oops
+ */
 
-require_once("Oops/Session/Abstract.php");
-require_once("Oops/Session/Interface.php");
+require_once ("Oops/Session/Abstract.php");
+require_once ("Oops/Session/Interface.php");
 
 class Oops_Session_Database extends Oops_Session_Abstract implements Oops_Session_Interface {
-
+	
 	/**
 	 * @var string Initial session data being read as session starts
 	 */
-	private $_savedData='';
+	private $_savedData = '';
 	
 	private $_tableSessions = 'sessions';
-	
+
 	public function _read($ses_id) {
-		$ses_id=preg_replace('/\W+/','',$ses_id);
+		$ses_id = preg_replace('/\W+/', '', $ses_id);
 		if(!strlen($ses_id)) return;
 		$result = Oops_Sql::Query("SELECT ses_value FROM {$this->_tableSessions} WHERE ses_id = '$ses_id'", true);
 		list($ses_data) = mysql_fetch_row($result);
-		$this->_savedData=$ses_data;
+		$this->_savedData = $ses_data;
 		return $ses_data;
 	}
-  
+
 	public function _write($ses_id, $data) {
 		if($data == $this->_savedData) return;
-		$ses_id = preg_replace('/\W+/','',$ses_id);
-		if (!strlen($ses_id)) return;
-
+		$ses_id = preg_replace('/\W+/', '', $ses_id);
+		if(!strlen($ses_id)) return;
+		
 		// @todo use User::setId algo here
-		if(Oops_Loader::load("Oops_User_Helper")) {
+		if(class_exists('Oops_User_Helper')) {
 			$user_id = Oops_User_Helper::GetID();
-		} else $user_id = 0;
-
+		} else
+			$user_id = 0;
+		
 		$ses_time = date("Ymd");
 		//Oops_Sql::Query("SET SQL_LOG_BIN=0");
 		if(strlen($data)) {
-			$data = Oops_Sql::Escape($data);
-			$query = "INSERT INTO {$this->_tableSessions} (ses_id, ses_time, ses_start, ses_value, user_id)
-				VALUES ('$ses_id', '$ses_time', '$ses_time', '$data', '$user_id')
-				ON DUPLICATE KEY UPDATE ses_time = '$ses_time', ses_value='$data', user_id = '$user_id'";
-			Oops_Sql::Query($query);
+			Oops_Sql_Common::insertUpdate($this->_tableSessions, array(
+				'ses_id' => $ses_id, 
+				'ses_time' => $ses_time, 
+				'ses_start' => $ses_time, 
+				'data' => $data, 
+				'user_id' => $user_id), array('ses_id'));
 		} else {
-			Oops_Sql::Query("DELETE FROM {$this->_tableSessions} WHERE ses_id='$ses_id'");
+			Oops_Sql_Common::delete($this->_tableSessions, array('ses_id' => $ses_id));
 		}
 		//Oops_Sql::Query("SET SQL_LOG_BIN=1");
 		return true;
 	}
-  
+
 	public function _destroy($ses_id) {
-		$ses_id=preg_replace('/\W+/','',$ses_id);
-		Oops_Sql::Query("DELETE FROM {$this->_tableSessions} WHERE ses_id = '$ses_id'");
+		$ses_id = preg_replace('/\W+/', '', $ses_id);
+		Oops_Sql_Common::delete($this->_tableSessions, array('ses_id' => $ses_id));
 		return true;
 	}
 
@@ -65,8 +67,8 @@ class Oops_Session_Database extends Oops_Session_Abstract implements Oops_Sessio
 	public function _gc($life) {
 	}
 
-        
 	// var $dbName = OOPS_SESSIONS_DB;
+	
 
 	/**
 	 * @param Oops_Config Session configuration
@@ -74,7 +76,7 @@ class Oops_Session_Database extends Oops_Session_Abstract implements Oops_Sessio
 	 */
 	public function __construct($config) {
 		parent::__construct($config);
-
+		
 		if($config->table) $this->_tableSessions = $config->table;
 		if($config->database) $this->_tableSessions = $config->database . '.' . $this->_tableSessions;
 		$this->setHandler();

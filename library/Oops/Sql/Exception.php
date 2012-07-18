e2862081b12d52b5edde9af271a3ac29031ed7fb
rockmagic.net <rockmagic.net@gmail.com>
2012-07-18 12:52:35 +0000

    Oops_Sql::Error now throws Oops_Sql_Exception

--- library/Oops/Sql/Exception.php
+++ library/Oops/Sql/Exception.php
@@ -1,5 +1,11 @@
 <?php
 
 class Oops_Sql_Exception extends Exception {
-	
+	public $query;
+
+	public function __construct($message, $code, $query = null) {
+		parent::__construct($message, $code);
+		$this->query = $query;
+	}
+
 }

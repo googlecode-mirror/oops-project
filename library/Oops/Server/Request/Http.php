8111fe1b1d4ea084c326f06efd7e95fb63f9b9e8
rockmagic.net <rockmagic.net@gmail.com>
2015-01-14 07:37:04 +0000

    HTTPS now sets for scheme when needed

--- library/Oops/Server/Request/Http.php
+++ library/Oops/Server/Request/Http.php
@@ -34,27 +34,30 @@ class Oops_Server_Request_Http extends Oops_Server_Request {
 		}
 		
 		$this->_proceedRequestFiles($_FILES, $this->_files);
-
+		
 		$this->_params = array_merge($this->_get, $this->_post, $this->_files);
-
+		
 		$parsed = parse_url($_SERVER['REQUEST_URI']);
-		foreach($parsed as $name=>$value) $this->$name = $value;
-
+		foreach($parsed as $name => $value)
+			$this->$name = $value;
+		
 		if(!isset($this->host)) $this->host = $_SERVER['HTTP_HOST'];
 		if(!isset($this->port)) $this->port = $_SERVER['SERVER_PORT'];
 		if(!isset($this->user) && isset($_SERVER['PHP_AUTH_USER'])) $this->user = $_SERVER['PHP_AUTH_USER'];
 		if(!isset($this->pass) && isset($_SERVER['PHP_AUTH_PW'])) $this->pass = $_SERVER['PHP_AUTH_PW'];
-
+		if(isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') $this->scheme = 'https';
 	}
 
 	/**
 	 * Transform incomming files array
-	 * 
+	 *
 	 * @todo Use objects to represent files
 	 * @todo Handle asyncronous file uploads
-	 *
-	 * @param $files array Received files array, as formatted by PHP
-	 * @param $keys array Current request keys stack
+	 *      
+	 * @param $files array
+	 *        	Received files array, as formatted by PHP
+	 * @param $keys array
+	 *        	Current request keys stack
 	 * @return void
 	 */
 	protected function _proceedRequestFiles($from, &$to) {
@@ -66,30 +69,29 @@ class Oops_Server_Request_Http extends Oops_Server_Request {
 					require_once 'Oops/File/Uploaded.php';
 					$to[$k] = new Oops_File_Uploaded($v);
 				}
-
 			} else {
 				if(!isset($to[$k]) || !is_array($to[$k])) $to[$k] = array();
 				$subfiles = array();
 				foreach(array_keys($v['name']) as $rk) {
 					$subfiles[$rk] = array(
-						'name' => $from[$k]['name'][$rk],
-						'type' => $from[$k]['type'][$rk],
-						'tmp_name' => $from[$k]['tmp_name'][$rk],
-						'error' => $from[$k]['error'][$rk],
-						'size' => $from[$k]['size'][$rk],
-					);
+						'name' => $from[$k]['name'][$rk], 
+						'type' => $from[$k]['type'][$rk], 
+						'tmp_name' => $from[$k]['tmp_name'][$rk], 
+						'error' => $from[$k]['error'][$rk], 
+						'size' => $from[$k]['size'][$rk]);
 				}
-				$this->_proceedRequestFiles($subfiles,$to[$k]);
+				$this->_proceedRequestFiles($subfiles, $to[$k]);
 			}
 		}
 	}
-	
+
 	protected function _stripSlashesRecursive(array &$var) {
 		if(!is_array($var)) return;
-		foreach($var as $k=>$v) {
-			if(is_string($var[$k])) $var[$k] = stripslashes($v);
-			elseif(is_array($var[$k])) $this->_stripSlashesRecursive($var[$k]);
+		foreach($var as $k => $v) {
+			if(is_string($var[$k]))
+				$var[$k] = stripslashes($v);
+			elseif(is_array($var[$k]))
+				$this->_stripSlashesRecursive($var[$k]);
 		}
 	}
-
 }
